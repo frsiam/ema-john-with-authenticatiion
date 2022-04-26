8618789434ade54410bbd43ac616e578e725b8b8
frsiam <siam.frs@gmail.com>
2022-04-26 14:41:06 +0600

    solve the issue of change page in review order

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import useProducts from '../../hooks/useProducts';
-import { addToDb, getStoredCart } from '../../utilities/fakedb';
+import useCart from '../../hooks/useCart';
+import { addToDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
-    const [cart, setCart] = useState([])
+    const [cart, setCart] = useCart()
     const [pageCount, setPageCount] = useState(0);
     const [page, setPage] = useState(0);
     const [size, setSize] = useState(10);
@@ -17,7 +17,7 @@ const Shop = () => {
         fetch(`http://localhost:4000/product?page=${page}&size=${size}`)
         .then(res => res.json())
         .then(data => setProducts(data))
-    },[page, size])
+    }, [page, size])
     
     useEffect(() => {
         fetch('http://localhost:4000/productcount')
@@ -27,20 +27,6 @@ const Shop = () => {
             setPageCount(pages);
         })
     },[])
-
-    useEffect(()=>{
-        const storedCart = getStoredCart()
-        const savedCart = []
-        for(const id in storedCart){
-            const addedProduct = products.find(product => product._id === id)
-            if(addedProduct){
-                const quantity = storedCart[id]
-                addedProduct.quantity = quantity
-                savedCart.push(addedProduct)
-            }
-        }
-        setCart(savedCart)
-    }, [products])
 
     const handleAddToCart = (selectedProduct) =>{
         let newCart = []
@@ -67,6 +53,7 @@ const Shop = () => {
                 <div className='pagination'>
                     {
                         [...Array(pageCount).keys()].map(number => <button
+                        key={number}
                         className={page === number ? 'selected' : ''}
                         onClick={() => setPage(number)}>{number + 1}</button>)  
                     }
